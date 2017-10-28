7dbe1884a28034393d6f93bd93b221f33a95c86b
Daniel Strokis <dstrokis@icloud.com>
2017-10-28 18:01:33 -0400

    Reverted the change made to skipSpace

--- CSS/cssprettyprint.c
+++ CSS/cssprettyprint.c
@@ -19,8 +19,12 @@ char next(int i, char *line, size_t linelen) {
 }
 
 void skipSpace(int *i, char *line, size_t linelen) {
-    while (isspace(next(++(*i), line, linelen)))
-        ;
+    char c = next(*i, line, linelen);
+    
+    while (isspace(c)){
+        *i += 1;
+        c = next(*i, line, linelen);
+    }
 }
 
 void addNewLine(int i, char *line, size_t linelen, char *out) {
