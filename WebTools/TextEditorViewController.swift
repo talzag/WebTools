d8ddc0434bedb0ce3c929022d7f48eead6b096b8
Daniel Strokis <dstrokis@icloud.com>
2017-11-24 18:24:24 -0500

    Removed simple instruction images from WebTools main window
    Created view controller to manage text view

---
+++ WebTools/TextEditorViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TextEditorViewController.swift
+//  WebTools
+//
+//  Created by Daniel Strokis on 11/24/17.
+//  Copyright © 2017 dstrokis. All rights reserved.
+//
+
+import Cocoa
+
+class TextEditorViewController: NSViewController {
+
+    @IBOutlet var textView: NSTextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+    
+}
